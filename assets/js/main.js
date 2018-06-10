81e0defe00bb56771b219cc1d86dc2aa4cfb94cf
Mario Garcia <iscmariog@gmail.com>
2018-06-10 16:08:56 -0500

    Content updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
 function descripcion(id) {
   id = id - 1;
   document.getElementById("nombre").innerHTML = mydata[id].nombre;
-  document.getElementById("direccion").innerHTML = mydata[id].localidad + "," + mydata[id].municipio;
+  document.getElementById("direccion").innerHTML = mydata[id].localidad + ", " + mydata[id].municipio;
   document.getElementById("telefono").innerHTML = mydata[id].telefono;
   document.getElementById("correo").innerHTML = mydata[id].correo;
   document.getElementById("sitio").innerHTML = mydata[id].sitio;
+  document.getElementById("nombre2").innerHTML = mydata[id].nombre;
   UIkit.modal('#descripcion').toggle();
 }
 
 var mymap = L.map('mapid').setView([20.9754, -89.6170], 12);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWF0dGRhcmsiLCJhIjoiY2l1N2szandzMDAwbzJvcW0xeTl1cXo5YyJ9.TWfy2tOwgTDV6u4GI7POgQ', {
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWF0dGRhcmsiLCJhIjoiY2ppOTRqMGg2MHN3eTNxcXRmdGsybHl3MCJ9.3mtdy_SHCJ-KvNWkJa8MGA', 
+{
   maxZoom: 18,
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
     '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -23,13 +25,13 @@ for (i = 0; i < mydata.length; i++) {
   markers[escuela.id] = L.marker([escuela.latitud, escuela.longitud]).addTo(mymap);
   markers[escuela.id]._icon.id = escuela.id;
 }
-for (var i = 0; i < mydata.length; i++){
-    m = document.getElementById(i+1);
-    if (typeof window.addEventListener === 'function'){
-        (function (_m) {
-            m.addEventListener('click', function(){
-                descripcion(_m.id);
-            });
-        })(m);
-    }
+for (var i = 0; i < mydata.length; i++) {
+  m = document.getElementById(i+1);
+  if (typeof window.addEventListener === 'function') {
+    (function (_m) {
+      m.addEventListener('click', function() {
+        descripcion(_m.id);
+      });
+    })(m);
+  }
 }
